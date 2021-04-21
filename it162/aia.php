95d0966afc62c30aa61c0bd709cc8bb10167ccac
keto3000 <77385097+keto3000@users.noreply.github.com>
2021-04-21 16:39:22 -0700

    add portal project files

---
+++ it162/aia.php
@@ -0,0 +1,7 @@
+<?php include "includes/header.php";?>
+
+      <h2 class="subheader">Template</h2>
+    <p> Use this is model for creating additional pages</p>
+        
+ 
+<?php include "includes/footer.php";?>
---
+++ it162/index.php
@@ -0,0 +1,29 @@
+<?php include "includes/header.php";?>
+          
+<!-- START LEFT COL -->
+<section>
+  <h2 class="pageID">Welcome</h2>
+    <img src="images/desktop.jpg" class="desktop" alt="Doorway with heart graffiti and quote" />
+    <img src="images/phone.jpg" class="phone" alt="" />
+ 
+      <p>I love meeting new people and discovering new places!  I've backpacked most of the United States, all of Canada and Puerto Rico.</p>
+      <p>As a lifelong student of anthropology & archaeology, patterns of culture and human ecology are my passion.</p>
+      <p>A few key travel priorites still on my bucket list include treks to visit Olduvai Gorge & the Serengeti plains of East Africa; Turkey's ancient archeological sites including Gobekli Tepe (the world oldest temple) and a touring trip of medieval historical sights in Prague and Eastern Europe.</p>
+      <p>My 20+ years working in the hospitality/event management industry in 3 major US cities has allowed me to work with a broad spectrum of people and global cultures. I'm currently pursuing a career in web development & design while honing my skills in creative content writing.</p>
+      <p>An ideal career environment for me would combine my love of travel with my passion to write and work on creative web design & development projects in the fields of hospitality, cultural events, museums and archaeological tourism.</p>
+  </section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+  <aside>
+    <img src="images/tablet.jpg" class="tablet" alt="hot air ballooning over Cappadocia, Turkey" />
+      <h3>My Travel "Bucket List"</h3>
+        <ul>
+          <li><a href="https://www.lonelyplanet.com/turkey/cappadocia-kapadokya">Welcome to Cappadocia</a></li>
+          <li><a href="https://whc.unesco.org/en/list/39">Ngorongoro Conservation Area</a></li>
+          <li><a href="https://www.medievalists.net/2020/05/eastern-europe-middle-ages/">Eastern Europe in the Middle Ages</a></li>
+        </ul>
+  </aside>
+<!-- END RIGHT COL -->
+ 
+<?php include "includes/footer.php";?>
