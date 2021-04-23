b4eb6ba307f7da564d1602e265b8fa8bb30111eb
keto3000 <77385097+keto3000@users.noreply.github.com>
2021-04-23 02:58:55 -0700

    corrected typo's on <p> tag for template

--- it162/template.php
+++ it162/template.php
@@ -1,6 +1,6 @@
 <?php include "includes/header.php";?>
 
       <h2 class="subheader">Template</h2>
-    <p> Use this as a model for creating additional pages</p>
+    <p>Use this as a model for creating additional pages</p>
  
 <?php include "includes/footer.php";?>
